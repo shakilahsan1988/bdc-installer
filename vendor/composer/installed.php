32a90e16c202bd8ebe91be79e7de83ea4002e9b0
Md Imran <mdimran24hrs@gmail.com>
2024-11-12 17:15:10 +0600

    package name change

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bdcoder/installer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1a2f46031dfae54bc4bc1721597e452a253fc24d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bdcoder/installer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1a2f46031dfae54bc4bc1721597e452a253fc24d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
